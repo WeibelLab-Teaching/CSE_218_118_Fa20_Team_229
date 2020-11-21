62d8b4dad52d4ae4f850f734ae1b5dc7185fdf8b
ShYuPe <shenyuepeng@bupt.edu.cn>
2020-11-21 10:52:20 +0800

    initial commit

---
+++ colyseus-babylonjs-boilerplate-master/server/src/entities/Player.ts
@@ -0,0 +1,19 @@
+import { Schema, type } from "@colyseus/schema";
+
+export interface PressedKeys {
+    x: number;
+    y: number;
+}
+
+export class Position extends Schema {
+    @type("number") x: number = 0;
+    @type("number") y: number = 0;
+    @type("number") z: number = 0;
+}
+
+export class Player extends Schema {
+    @type("string") name: string;
+    @type(Position) position = new Position();
+
+    pressedKeys: PressedKeys = { x: 0, y: 0 };
+}
